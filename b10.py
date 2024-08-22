82ef1df79e2db76c9335d58e70cc69155a3234ee
PhamThiVanAnh <phamanhmc2020@gmail.com>
2024-08-22 20:04:23 +0700

    Add files via upload

---
+++ b10.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 19:26:33 2024
+
+@author: VanAnh
+"""
+str_name=("Đại học quốc gia, khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM")
+print(str_name.replace(", ","\n").replace(", ","\n").replace(", ","\n"))
